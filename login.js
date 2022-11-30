79ceb9a209beb810c9170244789301fb428e0095
Kyle Tran <117237191+kyletran191@users.noreply.github.com>
2022-11-30 21:28:45 +0700

    Update login.js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ form.onsubmit = (e)=>{
 
 continueBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/php/login.php", true);
+    xhr.open("POST", "/api/login", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
